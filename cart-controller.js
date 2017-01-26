d561b72951237992b585f88d3809ed7be23a05fd
Sokolies <maxenc.brunel@gmail.com>
2017-01-26 21:48:13 +0100

    Test modal materialize

--- cart-controller.js
+++ cart-controller.js
@@ -2,15 +2,14 @@
 
 angular.module('chairApp')
 .controller('CartController', ['$scope', 'localStorage', '$http', function ($scope, localStorage, $http) {
-
 	var cart = 'cart';
 	var apiURL = 'http://localhost:1337/api';
 	$scope.cart = localStorage.get(cart);
 	$scope.form = {};
-	// $scope.form.titulary = "JOJO";
-	// $scope.form.number = 23434351513;
-	// $scope.form.expiration = "16/05/2017";
-	// $scope.form.cryptogram = '';
+	$scope.form.titulary = "JOJO";
+	$scope.form.number = 2343435151311254;
+	$scope.form.expiration = "16/05/2017";
+	$scope.form.cryptogram = '456';
 
 	$scope.deleteItem = function(reference){
 		localStorage.remove(reference);
@@ -26,7 +25,6 @@ angular.module('chairApp')
 	$scope.pay = function(){
 		$http.post(apiURL + '/paiment',$scope.form).then(function(res){
 			if( res.data.valid ){
-				console.log('Commande validée');
 			}
 			else{
 				console.log("Erreur : " +  res.data.message);
