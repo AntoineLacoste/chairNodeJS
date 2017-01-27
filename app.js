e6bea8e63d70fc6ddf8c39be675058f560dd36be
Maxence Brunel <maxenc.brunel@gmail.com>
2017-01-27 13:54:45 +0100

    Ajout de la modal lors d'un paiement

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('chairApp', ['ngRoute', 'ui.bootstrap'])
+var app = angular.module('chairApp', ['ngRoute'])
 .run(function($rootScope) {
     $rootScope.apiURL = 'http://localhost:1337/api';
 })
--- cart-controller.js
+++ cart-controller.js
@@ -8,14 +8,29 @@ angular.module('chairApp')
 	$scope.form.titulary = "JOJO";
 	$scope.form.number = 2343435151311254;
 	$scope.form.expiration = "16/05/2017";
+
 	$scope.form.cryptogram = '456';
 
+	// Création de la modal
+	$(document).ready(function(){
+		$('.modal').modal({
+			dismissible: true, 
+			opacity: .5,
+			outDuration: 200,
+			inDuration: 300,
+			startingTop: '4%',
+			endingTop: '10%'
+		});
+	});
+
+	// Suppression d'un item du panier
 	$scope.deleteItem = function(reference){
 		localStorage.remove(reference);
 		$scope.cart = localStorage.get(cart);
 		$scope.numberOfItems = localStorage.getNumberOfItems();
 	};
 
+	// Misa à jour de la quantité sur un produit
 	$scope.refreshCart = function(reference, qty){
 		localStorage.refresh(reference, qty);
 		$scope.numberOfItems = localStorage.getNumberOfItems();
@@ -23,10 +38,13 @@ angular.module('chairApp')
 
 	$scope.pay = function(){
 		$http.post($rootScope.apiURL + '/paiment',$scope.form).then(function(res){
+			$scope.validOrder = res.data.valid;
 			if( res.data.valid ){
+				$('#modal1').modal('open');
 			}
 			else{
-				console.log("Erreur : " +  res.data.message);
+				$scope.checkoutErrorMessage = res.data.message;
+				$('#modal1').modal('open');
 			}
 		}, function(err){
 			console.log('Erreur : ' + err);
