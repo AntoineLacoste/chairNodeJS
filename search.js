2af86fcd5ab1949b3b59d08c08c7cf17361658df
Maxence Brunel <maxenc.brunel@gmail.com>
2017-01-29 17:45:45 +0100

    Début filtre (initialisation + affichage)

--- search.js
+++ search.js
@@ -6,11 +6,21 @@ angular.module('chairApp')
 		templateUrl : 'search.html',
 		restrict: 'E',
 		controller : ['$scope', '$rootScope', function($scope, $rootScope){
-			$scope.resetItems = function(){
+			var newItems = [];
+			$scope.itemsColor = [];
+			$scope.itemsType = [];
+
+			// Initialisation de l'accordéon du filtre de recherche
+			$(document).ready(function(){
+				$('.collapsible').collapsible();
+			});
+
+			// Mise à jour des résultats lors d'une recherche 
+			$scope.searchItems = function(){
 				if($scope.search != ''){
-					var newItems = [];
+					newItems = [];
 					angular.forEach($rootScope.items, function(item){
-						if(item.name.toLowerCase().search($scope.search) >= 0){
+						if(item.name.toLowerCase().search($scope.search.toLowerCase()) >= 0){
 							newItems.push(item);
 						}
 					});
@@ -19,6 +29,26 @@ angular.module('chairApp')
 				else{
 					$scope.itemsToDisplay = $rootScope.items;
 				}
+			}
+
+			// Initialisation des filtres
+			$scope.initFilter = function(){
+				angular.forEach($rootScope.items, function(item){
+					console.log($scope.itemsColor, '   ', item.color.toLowerCase());
+					if( $scope.itemsColor.indexOf(item.color.toLowerCase()) < 0){
+						console.log($scope.itemsColor, '   ', item.color.toLowerCase());
+						$scope.itemsColor.push(item.color.toLowerCase());
+					}
+					if( $scope.itemsType.indexOf(item.type.toLowerCase()) < 0){
+						$scope.itemsType.push(item.type.toLowerCase());
+					}
+				});
+				console.log($scope.itemsColor);
+			}
+
+			// Mise à jour des résultats lors du filtre
+			$scope.filterItems = function(){
+
 			}
 		}]
 	};
