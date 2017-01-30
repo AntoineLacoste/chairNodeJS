f68b4ea61cb1a16aa0371f6cfdf530435955e97b
Maxence Brunel <maxenc.brunel@gmail.com>
2017-01-30 01:23:42 +0100

    Avancement filtre (En cours)

--- search.js
+++ search.js
@@ -7,8 +7,14 @@ angular.module('chairApp')
 		restrict: 'E',
 		controller : ['$scope', '$rootScope', function($scope, $rootScope){
 			var newItems = [];
-			$scope.itemsColor = [];
-			$scope.itemsType = [];
+			$scope.filters = [];
+			$scope.filters['type'] = [];
+			$scope.filters['color'] = [];
+
+			var filtersSelected = [];
+			filtersSelected['type'] = [];
+			filtersSelected['color'] = [];
+
 
 			// Initialisation de l'accordéon du filtre de recherche
 			$(document).ready(function(){
@@ -34,21 +40,40 @@ angular.module('chairApp')
 			// Initialisation des filtres
 			$scope.initFilter = function(){
 				angular.forEach($rootScope.items, function(item){
-					console.log($scope.itemsColor, '   ', item.color.toLowerCase());
-					if( $scope.itemsColor.indexOf(item.color.toLowerCase()) < 0){
-						console.log($scope.itemsColor, '   ', item.color.toLowerCase());
-						$scope.itemsColor.push(item.color.toLowerCase());
+					if( $scope.filters.color.indexOf(item.color.toLowerCase()) < 0){
+						$scope.filters.color.push(item.color.toLowerCase());
 					}
-					if( $scope.itemsType.indexOf(item.type.toLowerCase()) < 0){
-						$scope.itemsType.push(item.type.toLowerCase());
+					if( $scope.filters.type.indexOf(item.type.toLowerCase()) < 0){
+						$scope.filters.type.push(item.type.toLowerCase());
 					}
 				});
-				console.log($scope.itemsColor);
 			}
 
 			// Mise à jour des résultats lors du filtre
-			$scope.filterItems = function(){
+			$scope.filterItems = function(index, searchFilter){
 
+				// Mise à jour des filtres sélectionés
+				if( filtersSelected[searchFilter].indexOf($scope.filters[searchFilter][index]) >= 0){
+					filtersSelected[searchFilter].splice(index, 1);
+				}
+				else{
+					filtersSelected[searchFilter].push($scope.filters[searchFilter][index]);
+				}
+				// Recherche dans tous le items les articles correspondant aux filtres sélectionés
+				newItems = [];
+				angular.forEach($rootScope.items, function(item){
+					var key = '';
+					// On boucle sur les différents filtres 
+					for( key in filtersSelected ){
+						if(filtersSelected[key].length > 0){
+							// On vérifie que l'item courant contient une des occurences cochées du filtre courant
+							if( filtersSelected[key].indexOf( item[searchFilter].toLowerCase() ) >=0 ){
+								newItems.push(item);
+							}
+						}
+					}
+				});
+				$scope.itemsToDisplay = newItems;
 			}
 		}]
 	};
