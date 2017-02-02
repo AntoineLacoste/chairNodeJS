35ea992af9796aff996748d6b53028fdf11c7284
AntoineLacoste <antoine.lacoste@ynov.com>
2017-02-02 11:48:50 +0100

    remove filterByCaterogry from master

--- directives/search.js
+++ directives/search.js
@@ -7,14 +7,6 @@ angular.module('chairApp')
 		restrict: 'E',
 		controller : ['$scope', '$rootScope', function($scope, $rootScope){
 			var newItems = [];
-			$scope.filters = [];
-			$scope.filters['type'] = [];
-			$scope.filters['color'] = [];
-
-			$scope.selection = {
-				color: '',
-				type: ''
-			}
 
 
 			// Initialisation de l'accordéon du filtre de recherche
@@ -36,29 +28,7 @@ angular.module('chairApp')
 				else{
 					$scope.itemsToDisplay = $rootScope.items;
 				}
-			}
-
-			// Initialisation des filtres
-			$scope.initFilter = function(){
-				angular.forEach($rootScope.items, function(item){
-					if( $scope.filters.color.indexOf(item.color.toLowerCase()) < 0){
-						$scope.filters.color.push(item.color.toLowerCase());
-					}
-					if( $scope.filters.type.indexOf(item.type.toLowerCase()) < 0){
-						$scope.filters.type.push(item.type.toLowerCase());
-					}
-				});
-			}
-
-			// Mise à jour des résultats lors du filtre
-			$scope.toto = function(){
-				console.log('toto');
-				console.log('erreur ', $scope.test);
-			}
-
-			$scope.$watch('test', function(test) {
-				console.log(test);
-			});
+			};
 		}]
 	}
 }]);
